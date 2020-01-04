65c95b1e050f68bf4906643528901470e9b83ac8
Andrei Coroeanu <56926148+andrew-c0@users.noreply.github.com>
2020-01-04 16:24:35 +0200

    Prima versiune a aplicatiei pe frontend
    Login + url-uri corecte

---
+++ application/controllers/Login.php
@@ -0,0 +1,48 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+    public function index()
+    {
+        $this->load->view('elements/header.php');
+        $this->load->view('login.php');
+        $this->load->view('elements/footer.php');
+    }
+
+    public function check_login(){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        // verificarea daca exista in baza de date username
+        $check_user = $this->db->query('SELECT * FROM users WHERE username = "'.$username.'"');
+        if($check_user->num_rows() > 0){
+            // verificarea daca exista in baza de date username cu acea parola
+            $check_user_password = $this->db->query('SELECT * FROM users WHERE username = "'.$username.'" AND password = SHA1("'.$password.'")');
+            if($check_user_password->num_rows() > 0){
+                echo 'User found!';
+                $login = array(
+                    'username' => $username,
+                    'nume_user' => $check_user_password->result()[0]->nume_user
+                );
+
+                $this->session->set_userdata($login);
+
+                redirect('welcome', 'refresh');
+            }else{
+                echo 'Wrong password!';
+            }
+        }else{
+            echo 'User not found!';
+        }
+    }
+
+    public function logout(){
+        session_destroy();
+
+        redirect('login', 'refresh');
+    }
+}
+
+?>
